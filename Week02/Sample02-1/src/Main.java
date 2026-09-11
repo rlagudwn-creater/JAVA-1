f09bd3ece6529e928009901d950207b96ec0f2a2
rlagudwn-creater <rlagudwn@kbu.ac.kr>
2026-09-11 13:58:14 +0900

    2주차 수업 내용

---
+++ Week02/Sample02-1/src/Main.java
@@ -0,0 +1,15 @@
+
+    /*
+이 프로그램은 처음 실습 내용입니다.
+ */
+
+    void main() {
+        String name = "김형주";  // 문자열 데이터를 선언하는 선언문(대입문)\
+
+        System.out.printf("이름 : %s\n", name);
+        System.out.print("대한민국\n");
+        System.out.println("경복대학교");
+        System.out.println("김형주");
+
+    }
+}
---
+++ Week02/Sample02-2/src/Main.java
@@ -0,0 +1,16 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+void main() {
+    Scanner keyboard = new Scanner(System.in);
+    String name;
+    int age;
+
+    System.out.print("당신의 이름은 ? ");  //prompt 문장
+    name = keyboard.nextLine();
+    System.out.printf("%s의 나이은 ? ", name);  //prompt 문장
+    age = keyboard.nextInt();
+
+
+
+    System.out.printf("이름은 : %s, 나이는 : %d\n", name, age);
+}
